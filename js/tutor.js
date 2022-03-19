7f31af12726728fee8529bbf6551be7d62c39a17
Artem <sitkoartem2010@gmail.com>
2022-03-19 18:53:45 +0300

    cer

---
+++ js/tutor.js
@@ -0,0 +1,17 @@
+let red = 0
+let blue = 0
+let green = 0
+// setInterval(() => {
+//     if (red < 256) {
+//         for (red; red <= 255; red++) 
+//         {        console.log(1)
+//             document.querySelector('html').style.background = `rgb(${red},${green},${blue})`
+//         }
+//     }
+//     else {
+//         for (red; red >= 0; red--)   
+//         {console.log(2)
+//             document.querySelector('html').style.background = `rgb(${red},${green},${blue})`
+//         }
+//     }
+// })            
